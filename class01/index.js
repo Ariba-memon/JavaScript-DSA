3070599dc1b15f97b141df5eac4b65c1ddbf9bd1
Ariba-memon <aribaabdulqadir786@gmail.com>
2024-07-04 16:59:20 +0500

    class1

---
+++ class01/index.js
@@ -0,0 +1,71 @@
+// // console.log('Pastry Order Ariba') 
+
+// // setTimeout(() =>{
+// // console.log("Zulekha Cake Ready ")
+// // },4000)
+// // // console.log('Zulekha Order cake')
+
+// // console.log('Ayesha')
+
+// // //line by line
+// // //synchronus
+
+// // //asynchronus
+
+
+
+// function Ariba(){  //Parameter
+//     console.log("Memon")
+// }
+// Ariba() //arguments
+// //function call ya invoke
+
+
+
+// //callbacks functions
+
+// //Promises 
+// //Reject,Resolved,Pending
+
+// //Resolved 
+// //Pending 
+// //Reject
+
+// //async  await
+
+// //
+
+
+// var a = "Ariba"
+
+
+// var number = 123344444888888888888
+
+
+// var memon = "Ariba" // 1 , 2 ,3 ,4 ,5 //lenght
+// //0 ,1 ,2 ,3 ,4 //index
+
+
+// var members = ["Memon" ,"Ayesha",4,true] //string
+
+//string,number,boolean,undefined,null
+
+//undefined
+// var x;
+// console.log(x)
+
+
+// null
+// var a = null;
+// console.log(a)
+
+//concatenation //es5
+
+// var first_Name = "Ariba"
+// var second_Name = " Memon"
+// console.log(first_Name + second_Name)
+
+//es6 template literals
+// var first_Name = "Ariba"
+// var second_Name = " Memon"
+// console.log(`${first_Name} ${second_Name}`)
